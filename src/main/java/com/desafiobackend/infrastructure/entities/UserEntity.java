f5775258b4c194bdc6811964a0cbacd8bb06c605
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-12 14:12:13 -0300

    feat : add annotations to userEntity

--- src/main/java/com/desafiobackend/infrastructure/entities/UserEntity.java
+++ src/main/java/com/desafiobackend/infrastructure/entities/UserEntity.java
@@ -3,33 +3,31 @@ package com.desafiobackend.infrastructure.entities;
 
 import com.desafiobackend.domain.entities.user.User;
 import com.desafiobackend.domain.entities.user.UserType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Entity
+@Table(name= "tb_users")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of="id")
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String document;
+    @Column(unique = true)
     private String email;
     private String password;
     private BigDecimal balance;
-
+    @Enumerated(EnumType.STRING)
     private UserType userType;
 
     public static UserEntity mapUserToUserEntity(User user) {
