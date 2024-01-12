ce577e073277e7d23dc3b59a2c11b078edcde032
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-12 14:12:50 -0300

    refactor : add throws entityNotFoundException

--- src/main/java/com/desafiobackend/domain/repositories/UserRepository.java
+++ src/main/java/com/desafiobackend/domain/repositories/UserRepository.java
@@ -1,11 +1,12 @@
 package com.desafiobackend.domain.repositories;
 
 import com.desafiobackend.domain.entities.user.User;
+import jakarta.persistence.EntityNotFoundException;
 
 import java.util.List;
 
 public interface UserRepository {
     User createUser(User user);
     List<User> listUsers();
-    User findById(Long id) throws Exception;
+    User findById(Long id) throws EntityNotFoundException;
 }
