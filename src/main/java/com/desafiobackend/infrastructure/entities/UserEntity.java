831f0bbb1d0c5d154e4b6f7cb7136ff0cf6c5434
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-12 11:23:11 -0300

    git commit -m "feat: Add UserEntity entity for mapping between the domain and infrastructure layers"

--- src/main/java/com/desafiobackend/infrastructure/entities/UserEntity.java
+++ src/main/java/com/desafiobackend/infrastructure/entities/UserEntity.java
@@ -1,14 +1,51 @@
 package com.desafiobackend.infrastructure.entities;
 
 
+import com.desafiobackend.domain.entities.user.User;
+import com.desafiobackend.domain.entities.user.UserType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+    private String firstName;
+    private String lastName;
+    private String document;
+    private String email;
+    private String password;
+    private BigDecimal balance;
+
+    private UserType userType;
+
+    public static UserEntity mapUserToUserEntity(User user) {
+        UserEntity userEntity = new UserEntity();
+
+        userEntity.setFirstName(user.getFirstName());
+        userEntity.setLastName(user.getLastName());
+        userEntity.setDocument(user.getDocument());
+        userEntity.setEmail(user.getEmail());
+        userEntity.setPassword(user.getPassword());
+        userEntity.setBalance(user.getBalance());
+        userEntity.setUserType(user.getUserType());
+
+        return userEntity;
+
+    }
+
+
 }
