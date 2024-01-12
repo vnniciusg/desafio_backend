9df7f1a8cfb891289a7a70cf43145283b5de45b0
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-12 14:11:36 -0300

    feat : add transactional

--- src/main/java/com/desafiobackend/infrastructure/repositories/user/UserRepositoryImpl.java
+++ src/main/java/com/desafiobackend/infrastructure/repositories/user/UserRepositoryImpl.java
@@ -1,14 +1,15 @@
-package com.desafiobackend.infrastructure.repositories;
+package com.desafiobackend.infrastructure.repositories.user;
 
 import com.desafiobackend.domain.entities.user.User;
 import com.desafiobackend.domain.repositories.UserRepository;
 import com.desafiobackend.infrastructure.entities.UserEntity;
 import com.desafiobackend.infrastructure.mapper.user.UserMapper;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
@@ -23,23 +24,28 @@ public class UserRepositoryImpl implements UserRepository {
     }
 
     @Override
+    @Transactional
     public User createUser(User user) {
-        UserEntity userEntity = UserEntity.mapUserToUserEntity(user);
-        UserEntity userEntityCreated = this.infraUserRepository.save(userEntity);
-        return userMapper.toUser(userEntityCreated);
-
+        try{
+            UserEntity userEntity = UserEntity.mapUserToUserEntity(user);
+            UserEntity userEntityCreated = this.infraUserRepository.save(userEntity);
+            return userMapper.toUser(userEntityCreated);
+        }catch (Exception exception){
+            throw new RuntimeException("Erro ao criar usuário: " , exception);
+        }
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> listUsers() {
        List<UserEntity> userEntities = this.infraUserRepository.findAll();
-        return userMapper.toUsers(userEntities);
+       return userMapper.toUsers(userEntities);
     }
 
     @Override
-    public User findById(Long id) throws Exception{
-        UserEntity userEntity =  this.infraUserRepository.findById(id).orElseThrow(() -> new Exception("Usuario não encontrado"));
+    @Transactional(readOnly = true)
+    public User findById(Long id) throws EntityNotFoundException {
+        UserEntity userEntity =  this.infraUserRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuario não encontrado"));
         return userMapper.toUser(userEntity);
-
     }
 }
