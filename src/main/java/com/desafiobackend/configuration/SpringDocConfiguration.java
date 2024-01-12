2f556fc4bb8556f4e012950eb757090bcccde8fe
vnniciusg <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2024-01-12 14:12:23 -0300

    feat : add spring doc config

---
+++ src/main/java/com/desafiobackend/configuration/SpringDocConfiguration.java
@@ -0,0 +1,16 @@
+package com.desafiobackend.configuration;
+
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfiguration {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .components(new Components());
+    }
+}
